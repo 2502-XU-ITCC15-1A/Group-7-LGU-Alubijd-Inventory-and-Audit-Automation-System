e890952ce03addc207f37037f52f4632b1bc7fde
James Gica <jamesgica7@gmail.com>
2026-04-16 23:06:29 +0800

    Add files via upload

---
+++ ALIAS-main/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html') #change the file to its destination file
+
+@app.route('/inventory')
+def inventory():
+    return render_template('index.html') #change the file to its destination file
+
+@app.route('/audit')
+def audit():
+    return render_template('index.html') #change the file to its destination file
+
+@app.route('/history')
+def history():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
