a4e28f2f5f81d8e46e5847d3a144d8970a4232e1
James Gica <jamesgica7@gmail.com>
2026-05-18 10:11:57 +0800

    fixed mysql port for cloud hosting

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
     MYSQL_CURSORCLASS = "DictCursor"
 
     UPLOAD_FOLDER = os.path.join("static", "uploads")
